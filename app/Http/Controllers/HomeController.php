150412fc7fd04b2bff2d5861b0968e1eb4c917a9
maheshmirase2021 <mmahesh@trellissoft.com>
2021-12-16 01:00:30 +0530

    Laravel CRUD with Multiple Record Deletion

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Product;
+use DB;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->product  = new Product();
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        //return view('products.index');
+
+        try{
+            $products = $this->product->getProductsList();          
+            return view('products.index', compact('products'));
+        } catch (\Exception $e){
+            return response()->json(["status" => 400, "message" => $e->getMessage()]); 
+        } 
+
+        //return view('home');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    return view('auth.login');
+});
+
+Auth::routes();
+
+Route::group(['middleware' => ['auth']], function() {
+    
+    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+    Route::post('/addproduct', 'product\ProductController@store')->name('product\product.store');
+    Route::get('/product', 'product\ProductController@index')->name('product\product.index');
+    Route::post('/updateproduct','product\ProductController@update')->name('product\product.update');
+    Route::get('/deleteprod/{id}','product\ProductController@destroy')->name('product\product.destroy');
+  
+    Route::delete('/productsDeleteAll', 'product\ProductController@deleteAll')->name('product\product.deleteAll');
+
+});
+
